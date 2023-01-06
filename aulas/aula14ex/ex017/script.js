91a3572d2fa4e8eff086e4ff3358c06835625fe3
Rafael Duarte <115437853+rafaelmzdev@users.noreply.github.com>
2023-01-06 01:10:01 -0300

    novo exercício

--- aulas/aula14ex/ex017/script.js
+++ aulas/aula14ex/ex017/script.js
@@ -1,1 +1,17 @@
-alert('Olá')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if (num.value.length == 0){
+        alert('Por favor, digite um número')
+    } else {
+        let numero = Number(num.value)
+        tab.innerHTML = ``
+        for (i = 1; i <= 10; i++){
+            let item = document.createElement('option')
+            item.text = `${numero} x ${i} = ${numero*i}`
+            item.value = `tab${i}`
+            tab.appendChild(item)
+        }
+    }
+}
