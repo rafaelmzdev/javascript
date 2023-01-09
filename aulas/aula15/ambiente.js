5c877d2c4a326e5c290bfa2e25870664ad547686
Rafael Duarte <115437853+rafaelmzdev@users.noreply.github.com>
2023-01-09 00:24:53 -0300

    Update ambiente.js

--- aulas/aula15/ambiente.js
+++ aulas/aula15/ambiente.js
@@ -7,3 +7,6 @@ num.sort()
 for (let i = 0; i < num.length; i++){
     console.log(`A posição ${i} tem o número ${num[i]}`)
 }
+
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
